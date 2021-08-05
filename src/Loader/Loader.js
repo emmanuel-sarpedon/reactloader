754ff6606c5322418fbbd5f54f569b1226165ee5
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-05 14:56:28 +0400

    [FEAT](<Loader/>): add new kind of loader 'audio-wave'

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -1,7 +1,7 @@
 import "./Loader.scss";
 
 const Loader = (props) => {
-  const { type, size, borderWidth, color, animationDuration } = props;
+  const { type, size, borderWidth, color, animationDuration, bar } = props;
 
   const loaderType = type || "spinner";
 
@@ -11,13 +11,37 @@ const Loader = (props) => {
       <div
         className={loaderType}
         style={{
-          height: `${size && size}`,
-          width: `${size && size}`,
-          color: `${color && color}`,
           animationDuration: `${animationDuration && animationDuration}`,
           borderWidth: `${borderWidth && borderWidth}`,
+          color: `${color && color}`,
+          height: `${size && size}`,
+          width: `${size && size}`,
         }}
       ></div>
+    );
+  }
+
+  // AUDIO WAVE
+  if (loaderType === "audio-wave") {
+    const barDiv = [];
+    const numberOfBar = Math.max(bar, 5) || 5;
+
+    for (let i = 0; i < numberOfBar; i++) {
+      barDiv.push(
+        <div
+          className="bar"
+          style={{
+            animationDuration: `${animationDuration && animationDuration}`,
+            backgroundColor: `${color && color}`,
+            height: `${size && size}`,
+          }}
+        ></div>
+      );
+    }
+    return (
+      <div className={loaderType} style={{}}>
+        {barDiv}
+      </div>
     );
   }
 };
