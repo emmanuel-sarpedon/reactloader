03bad70ea5273a3fd65b3e19c2d935abcb368573
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-05 16:09:41 +0400

    [FEAT](<App/>): first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,37 @@ import Loader from "./Loader/Loader";
 function App() {
   return (
     <div className="app">
-      <Loader type="spinner" size="30px"  borderWidth="25px" color="lightblue" animationDuration="0.8s"/>
+      <div>
+        <Loader
+          type="spinner"
+          size="20px"
+          borderWidth="2px"
+          color="lightgrey"
+          animationDuration="0.8s"
+        />
+        <span>Spinner</span>
+      </div>
+
+      <div>
+        <Loader
+          type="audio-wave"
+          size="30px"
+          color="lightgrey"
+          animationDuration="0.8s"
+          numberOfItems="8"
+        />
+        <span>Audio wave</span>
+      </div>
+
+      <div>
+        <Loader
+          type="ellipsis-fall"
+          animationDuration="2s"
+          color="lightgrey"
+          numberOfItems="5"
+        />
+        <span>Ellipsis fall</span>
+      </div>
     </div>
   );
 }
